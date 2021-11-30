3d2fc6205e286cfcca6bf83f3fd353040295b5db
Josh Levine <josh.levine1@gmail.com>
2021-11-29 22:42:21 -0800

    Initial Commit

---
+++ Shared/Sorting_AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sorting_AlgorithmsApp.swift
+//  Shared
+//
+//  Created by Joshua Levine on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Sorting_AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
